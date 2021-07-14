756943eb53416f160e7e670d39457b50c192076c
JVPhase <john.vasiliev@gmail.com>
2021-07-14 05:13:56 +0300

    generate new component

---
+++ src/components/TextInput/TextInput.stories.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import TextInput from './TextInput';
+export default {
+  title: 'trui/TextInput',
+  component: TextInput
+};
+export const Template = () => <TextInput />;
